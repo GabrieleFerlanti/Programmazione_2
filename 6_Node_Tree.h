d800ae6c4193f796c9ed89431d89aa801b3c49ca
Gabriele Ferlanti <gabrieleferlanti205@gmail.com>
2023-11-27 16:45:35 +0100

    Node Tree

---
+++ 6_Node_Tree.h
@@ -0,0 +1,13 @@
+template <typename K, typename D>
+    class Node{
+        private:
+            K key;
+            D data;
+        public:
+            Node * left;
+            Node * right;
+            Node() : left(nullptr), right(nullptr){}
+            Node(K key, D data) : key(key), data(data), left(nullptr), right(nullptr){}
+            K getKey(){return key;}
+            D getData(){return data;}
+    };
